4a1109f4f1d5d5ff0e81d2e57ac01c939b2399b7
Wilder Trujillo <nadie437@gmail.com>
2022-06-15 16:48:12 -0500

    agregando validadores al post de users con express-validator

--- routes/users.route.js
+++ routes/users.route.js
@@ -14,6 +14,11 @@ router.get('/', getUsers)
 //adding middleware, if we need to use only one, it's not required to put it in an array
 router.post('/',[
   check('email', 'el correo no es válido').isEmail(), //this will send the error to the controller
+  check('name', 'El nombre es obligatorio').not().isEmpty(),
+  check('lastName', 'El apellido es obligatorio').not().isEmpty(),
+  check('password', 'La contraseña debe ser al menos 6 caracteres').isLength({ min: 6 }),
+  check('role', 'No es un rol válido').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+
 ], postUser)
 router.put('/:id', putUser)
 router.patch('/', patchUser)
